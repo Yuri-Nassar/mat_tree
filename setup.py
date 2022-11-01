ec38b104ab0c0b8215a1892e6ee03620a34e56e4
Yuri Santos <yuri.nassar@gmail.com>
2022-10-31 22:51:45 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mat_tree',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Projeto para o desenvolvimento de algoritmo para trajetórias multiaspectos com base em árvores.',
+    author='Yuri Santos, Ricardo e Jhonata Tyska',
+    license='MIT',
+)
