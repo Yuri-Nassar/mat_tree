6679f4cc3ef17f9a240fd89ac3bb64e149ca24d9
Yuri-Nassar <yuri.nassar@gmail.com>
2023-05-15 22:25:45 -0300

    add encoding format (ISO-8859-1) to read fsny dataset

--- scripts/utils.py
+++ scripts/utils.py
@@ -7,11 +7,11 @@ def get_dataset(df_name="fsny_10"):
         dtype = {'tid': 'int32', 'time': 'category', 'day': 'category', 'poi': 'category',
                  'type': 'category', 'root_type': 'category', 'rating': 'category',
                  'weather': 'category', 'label': 'category'}
-        return pd.read_csv(path+df_name+"/foursquare.csv", sep=';', dtype=dtype)
+        return pd.read_csv(path+df_name+"/foursquare.csv", sep=';', dtype=dtype, encoding="ISO-8859-1")
     
     elif df_name.split('_')[0] == 'basometro':
         return pd.read_csv(path+df_name+"/basometro.csv")
     
     else:
-        print('Dataset does not exist! Check the options.')
+        print(f"'{df_name}' dataset does not exist! Check the available options.")
 
